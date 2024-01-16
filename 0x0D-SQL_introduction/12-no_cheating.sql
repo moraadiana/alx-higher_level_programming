aa499d0ebb8eb3db1181391e13281dd3febe5644
moraadiana <97588455+moraadiana@users.noreply.github.com>
2024-01-16 21:23:19 +0300

    done

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,5 @@
+
+
 -- update data
-UPDATE second_table SET score = '10' where name = "Bob"
+-- Execute: cat 12-no_cheating.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+UPDATE `second_table` SET `score`=10 WHERE `second_table`.`name`="Bob";
--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,4 @@
--- removes all records with a score <= 5 in the table second_table
-delete * from second_table where score<=5 
+
+-- Delete rows
+-- Execute: cat 13-change_class.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+DELETE FROM second_table WHERE score <= 5;
