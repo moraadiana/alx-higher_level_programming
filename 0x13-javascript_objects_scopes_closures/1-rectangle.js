5dabe59b46ea463bc4f4098107543d7d6e073b98
moraadiana <97588455+moraadiana@users.noreply.github.com>
2024-02-13 17:20:39 +0300

    done

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
- class Rectangle {
-    constructor (w, h) {
-      this.width = w;
-      this.height = h;
+class Rectangle {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
     }
   };
