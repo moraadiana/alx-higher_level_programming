d3ce1ed98ccbf5a4f442a853ba54ecf0e09496bd
moraadiana <97588455+moraadiana@users.noreply.github.com>
2024-02-19 10:35:38 +0300

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-from sys import argv
+import sys
 
 # The code should not be executed when imported
 if __name__ == '__main__':
 
     # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
 
     # It gives us the ability to have multiple seperate working environments
     # through the same connection to the database.
